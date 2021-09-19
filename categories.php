00cef474c6b166073ccc3e5a5df9c2e5005584e9
Archiroo <newbieit.nb@gmail.com>
2021-09-19 17:48:01 +0700

    Lan 1

---
+++ categories.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Categories</title>
+</head>
+<body>
+    <section class="header">
+        <img src="image/logo.png"alt="">
+        <div class="navbar">
+            <a href="index.php">Home</a>
+            <a href="categories.php">Categories</a>
+            <a href="food.php">Foods</a>
+            <a href="order.php">Order</a>
+        </div>
+    </section>
+    <section class="explore-food">
+        <h2 class=>Explore-Foods</h2>
+        <div class="container">
+            <div class="box">
+                <img src="image/pizza.jpg" alt="">
+                <h3>Pizza</h3>
+            </div>
+            <div class="box">
+                <img src="image/burger.jpg" alt="">
+                <h3>Burger</h3>
+            </div>
+            <div class="box">
+                <img src="image/momo.jpg" alt="">
+                <h3>Momo</h3>
+            </div>
+            <div class="box">
+                <img src="image/pizza.jpg" alt="">
+                <h3>Pizza</h3>
+            </div>
+            <div class="box">
+                <img src="image/burger.jpg" alt="">
+                <h3>Burger</h3>
+            </div>
+            <div class="box">
+                <img src="image/momo.jpg" alt="">
+                <h3>Momo</h3>
+            </div>
+            <div class="box">
+                <img src="image/pizza.jpg" alt="">
+                <h3>Pizza</h3>
+            </div>
+            <div class="box">
+                <img src="image/burger.jpg" alt="">
+                <h3>Burger</h3>
+            </div>
+            <div class="box">
+                <img src="image/momo.jpg" alt="">
+                <h3>Momo</h3>
+            </div>
+        </div>
+    </section>
+    <div class="footer">
+        <div class="link-icon">
+            <a href="#">
+                <i class="footer-item__icon fab fa-facebook"></i>
+            </a>
+            <a href="">
+                <i class="footer-item__icon fab fa-instagram"></i>
+            </a>
+            <a href="">
+                <i class="footer-item__icon fab fa-twitter"></i>    
+            </a>
+        </div>
+        <h3>All rights reserverd. Design by <a href="http://www.tlu.edu.vn/" target="_blank">CSE.TLU</a></h3>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Nguyễn Minh Đức</title>
+</head>
+<body>
+    <section class="header">
+        <img src="image/logo.png"alt="">
+        <div class="navbar">
+            <a href="index.php">Home</a>
+            <a href="categories.php">Categories</a>
+            <a href="food.php">Foods</a>
+            <a href="order.php">Order</a>
+        </div>
+    </section>
+
+    <div class="food-search">
+        <div class="container">
+            <form action="">
+                <input type="search" placeholder="Search food" class="txt-search">
+                <input type="submit" value="Search" class="btn-search">
+            </form>
+        </div>
+    </div>
+
+    <section class="explore-food">
+        <h2 class=>Explore-Foods</h2>
+        <div class="container">
+            <div class="box">
+                <img src="image/pizza.jpg" alt="">
+                <h3>Pizza</h3>
+            </div>
+            <div class="box">
+                <img src="image/burger.jpg" alt="">
+                <h3>Burger</h3>
+            </div>
+            <div class="box">
+                <img src="image/momo.jpg" alt="">
+                <h3>Momo</h3>
+            </div>
+        </div>
+    </section>
+
+    <div class="food-menu">
+        <h2>Food Menu</h2>
+        <div class="container">
+            <div class="box">
+                <img src="image/menu-pizza.jpg" alt="">
+                <div class="food-menu-content">
+                    <h4>Food Title</h4>
+                    <p class="price">$2.3</p>
+                    <p class="detail">Made with Italian Sauce, Chicken, and organice vegetables</p>
+                    <a href="" class="btn-order">Order now</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="image/menu-burger.jpg" alt="">
+                <div class="food-menu-content">
+                    <h4>Food Title</h4>
+                    <p class="price">$2.3</p>
+                    <p class="detail">Made with Italian Sauce, Chicken, and organice vegetables</p>
+                    <a href="" class="btn-order">Order now</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="image/menu-momo.jpg" alt="">
+                <div class="food-menu-content">
+                    <h4>Food Title</h4>
+                    <p class="price">$2.3</p>
+                    <p class="detail">Made with Italian Sauce, Chicken, and organice vegetables</p>
+                    <a href="" class="btn-order">Order now</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="image/menu-pizza.jpg" alt="">
+                <div class="food-menu-content">
+                    <h4>Food Title</h4>
+                    <p class="price">$2.3</p>
+                    <p class="detail">Made with Italian Sauce, Chicken, and organice vegetables</p>
+                    <a href="" class="btn-order">Order now</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="image/menu-burger.jpg" alt="">
+                <div class="food-menu-content">
+                    <h4>Food Title</h4>
+                    <p class="price">$2.3</p>
+                    <p class="detail">Made with Italian Sauce, Chicken, and organice vegetables</p>
+                    <a href="" class="btn-order">Order now</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="image/menu-momo.jpg" alt="">
+                <div class="food-menu-content">
+                    <h4>Food Title</h4>
+                    <p class="price">$2.3</p>
+                    <p class="detail">Made with Italian Sauce, Chicken, and organice vegetables</p>
+                    <a href="" class="btn-order">Order now</a>
+                </div>
+            </div>           
+        </div>
+       <p class="text-bottom">
+           <a href="#">See all foods</a>
+       </p>
+    </div>
+
+    <div class="footer">
+        <div class="link-icon">
+            <a href="#">
+                <i class="footer-item__icon fab fa-facebook"></i>
+            </a>
+            <a href="">
+                <i class="footer-item__icon fab fa-instagram"></i>
+            </a>
+            <a href="">
+                <i class="footer-item__icon fab fa-twitter"></i>    
+            </a>
+        </div>
+        <h3>All rights reserverd. Design by <a href="http://www.tlu.edu.vn/" target="_blank">CSE.TLU</a></h3>
+    </div>
+</body>
+</html>
